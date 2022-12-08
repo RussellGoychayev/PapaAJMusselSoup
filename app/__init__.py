cdb00d2db5533a5f64755802d1877b577f8a68a9
James Yu <jyu30@stuy.edu>
2022-12-08 13:15:13 -0500

    api testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, render_template, session, request
 import sqlite3
+import requests
 
 app = Flask(__name__)
 
@@ -12,7 +13,14 @@ c.execute(command)
 
 @app.route('/', methods = ['GET', 'POST'])
 def login():
-	return render_template('abc.html')
+	url =  "https://api.edamam.com/api/recipes/v2"
+	res = requests.get(url, params={'type':'public', 'app_id':"904296dd", 'app_key':"58228c816ae6f1c88cca02d85c4da325", 'q': 'chicken'})
+	print(res.json()['hits'][0]['recipe']['label']) # prints the name of the first chicken recipe 
+	
+	url2 = "https://api.spoonacular.com/recipes/716429/information?apiKey=7081bf709f0d44b7984587105086357f"
+	res2 = requests.get(url2)
+	a = res2.json()['summary']
+	return render_template('abc.html', s=a)
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
